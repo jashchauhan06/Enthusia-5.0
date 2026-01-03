51445d345115dafe89c1123f65a01d66f6442136
Jash Chauhan <jashchauhan06@gmail.com>
2026-01-03 23:17:50 +0530

    fix

--- src/sections/home/content/availability-status.tsx
+++ src/sections/home/content/availability-status.tsx
@@ -67,15 +67,7 @@ export function AvailabilityStatus() {
 
       {/* Desktop version (>= 768px) */}
       <div className="hidden md:flex items-center gap-2 text-white">
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
         <span>Registration are not opened yet!</span>
-=======
-        <span>Get ready for ENTHUSIA 5.0!</span>
->>>>>>> Stashed changes
-=======
-        <span>Get ready for ENTHUSIA 5.0!</span>
->>>>>>> Stashed changes
         <span>•</span>
         <span>SIT Nagpur</span>
         <span>•</span>
